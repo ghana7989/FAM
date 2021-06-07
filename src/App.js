b507b20417e92d320fa6fa4b7ef786b6715ed488
Chindukuri Pavan <ghana.chindukuri@gmail.com>
2021-06-07 13:16:27 +0530

    💥 NEW: Just Getting Started

--- src/App.js
+++ src/App.js
@@ -1,25 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
+import Register from './pages/auth/Register'
+import Login from './pages/auth/Login'
+import Home from './pages/Home'
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<Switch>
+			<Route path="/" exact component={Home} />
+			<Route path="/login" exact component={Login} />
+			<Route path="/register" exact component={Register} />
+		</Switch>
+	)
 }
 
-export default App;
+export default App
